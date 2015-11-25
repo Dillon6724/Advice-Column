5f3db082b614c13fec78d638b71323e25807acf8
Dillon McGuire <DMMcGuire6724@gmail.com>
2015-11-24 19:14:03 -0500

    pre heroku

--- app/assets/javascripts/angular.js
+++ app/assets/javascripts/angular.js
@@ -1,14 +1,15 @@
 var app = angular.module('AdviceApp', []),
-		//Function to render all questions on to page
-		getQuestions = function (){
-			$http.get('/questions').then(function (data){
-				controller.data = data.data.questions
-			});
-		};
+		//Function will be set to render all questions on to page inside of Question controller
+		getQuestions;
 
 /////////////////// QUESTION CONTROLLER  //////////////////////		
 app.controller('QuestionsController', ['$http', function($http) {
 	var controller = this
+	getQuestions = function (){
+		$http.get('/questions').then(function (data){
+			controller.data = data.data.questions
+		});
+	};
 	getQuestions()
 	this.authenticity_token = document.querySelector('meta[name="csrf-token"]').getAttribute('content');
 	this.askQuestion = function () {
@@ -24,11 +25,13 @@ app.controller('QuestionsController', ['$http', function($http) {
 
 }]);
 
+//////////////////    NEW ANSWER CONTROLLER     ///////////////////
 app.controller('newAnswerController', ['$http', '$scope', function ($http, $scope) {
 	var controller = this
 	this.authenticity_token = document.querySelector('meta[name="csrf-token"]').getAttribute('content');
+	
 	this.addAnswer = function () {
-		$http.post('/answers/new/' + $scope.$parent.q.id, {
+		$http.post('/answers/new/' + $scope.$parent.q.id, { //$scope.$parent.q.id is equal to the ID of the specific question
 			authenticity_token: controller.authenticity_token,
 			answer: {
 				body: controller.newAnswerBody,
